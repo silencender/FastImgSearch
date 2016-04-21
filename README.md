f0c3c9b2fda273919cf24b569a8514ad0bd48832
silencender <silenceender@gmail.com>
2016-04-21 23:43:32 +0800

    The First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FastImgSearch
+已完成：拍照并自由裁切后保存
---
+++ simple/src/main/java/com/jph/simple/MainActivity.java
@@ -0,0 +1,70 @@
+package com.jph.simple;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.jph.takephoto.TakePhotoActivity;
+
+import java.io.File;
+
+/**
+ * 从相册选择照片进行裁剪，从相机拍取照片进行裁剪<br>
+ * 从相册选择照片（不裁切），并获取照片的路径<br>
+ * 拍取照片（不裁切），并获取照片路径
+ * @author JPH
+ * @Date:2014.10.09
+ */
+public class MainActivity extends TakePhotoActivity {
+    private ImageView imgShow;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imgShow= (ImageView) findViewById(R.id.imgShow);
+    }
+    public void cropPic(View view) {
+        //Uri imageUri = Uri.fromFile(new File(Environment.getDataDirectory(), "/temp/"+System.currentTimeMillis() + ".jpg"));
+        Uri imageUri = Uri.fromFile(new File("/storage/emulated/0/DCIM/Camera/"+System.currentTimeMillis() + ".jpg"));
+        switch (view.getId()) {
+            case R.id.btnCropFromGallery://从相册选择照片进行裁剪
+                getTakePhoto().picSelectCrop(imageUri);
+                break;
+            case R.id.btnCropFromTake://从相机拍取照片进行裁剪
+                getTakePhoto().picTakeCrop(imageUri);
+                break;
+            case R.id.btnOriginal://从相册选择照片不裁切
+                getTakePhoto().picSelectOriginal(imageUri);
+                break;
+            case R.id.btnTakeOriginal://从相机拍取照片不裁剪
+                getTakePhoto().picTakeOriginal(imageUri);
+                break;
+            default:
+                break;
+        }
+    }
+    @Override
+    public void takeCancel() {
+        super.takeCancel();
+    }
+    @Override
+    public void takeFail(String msg) {
+        super.takeFail(msg);
+    }
+    @Override
+    public void takeSuccess(Uri uri) {
+        super.takeSuccess(uri);
+        showImg(uri);
+        compressPic(uri.getPath());
+    }
+    private void showImg(Uri uri){
+        BitmapFactory.Options option=new BitmapFactory.Options();
+        option.inSampleSize=2;
+        Bitmap bitmap=BitmapFactory.decodeFile(uri.getPath(),option);
+        imgShow.setImageBitmap(bitmap);
+    }
+}
